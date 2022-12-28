3ec24282bc3d1dbd1c56267129077e0e1dd2c68e
rohan <rohan.vagadiya14@gmail.com>
2022-12-29 00:57:05 +0530

    add url endpoints for all data , for yearly data

---
+++ README.md
@@ -0,0 +1,6 @@
+# What is this project?
+this is a weather api containing some 
+weather data for regions in europe.
+this data was taken from EUROPEAN 
+CLIMATE ASSESSMENT & DATASET
+(ECA&D) that anyone can access.
--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def home():
 
 @app.route('/api/v1/<station>/<date>')
 def one_date_temp_data(station, date):
-    filepath = 'data_small_weather/TG_STAID' + str(station).zfill(6) + '.txt'
+    filepath = 'data/TG_STAID' + str(station).zfill(6) + '.txt'
     df = pd.read_csv(filepath, skiprows=20, parse_dates=['    DATE'])
     temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     content = {
@@ -27,7 +27,7 @@ def one_date_temp_data(station, date):
 
 @app.route('/api/v1/<station>')
 def all_data(station):
-    filepath = 'data_small_weather/TG_STAID' + str(station).zfill(6) + '.txt'
+    filepath = 'data/TG_STAID' + str(station).zfill(6) + '.txt'
     df = pd.read_csv(filepath, skiprows=20, parse_dates=['    DATE'])
     result = df.to_dict(orient='records')
     return result
@@ -35,7 +35,7 @@ def all_data(station):
 
 @app.route('/api/v1/yearly/<station>/<year>')
 def data_one_year(station, year):
-    filepath = 'data_small_weather/TG_STAID' + str(station).zfill(6) + '.txt'
+    filepath = 'data/TG_STAID' + str(station).zfill(6) + '.txt'
     df = pd.read_csv(filepath, skiprows=20)
     df['    DATE'] = df['    DATE'].astype(str)
     result = df[df['    DATE'].str.startswith(str(year))].to_dict(orient='record')
